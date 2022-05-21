782ea46b44ed4279ca6b626c0a8007ec24dcc287
Enes Dolgun <58950702+dlgn2@users.noreply.github.com>
2022-05-21 20:47:38 +0300

    d

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,46 @@
-import { printLine } from './modules/print';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '@webcomponents/custom-elements';
+import ContentScript from './ContentScript';
+import { StylesProvider, jssPreset } from '@material-ui/styles';
+import { create } from 'jss';
+import { claimPopup } from './main';
 
-console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
+class ReactExtensionContainer extends HTMLElement {
+  connectedCallback() {
+    const mountPoint = document.createElement('div');
+    mountPoint.innerHTML = claimPopup.stringPopUp;
 
-printLine("Using the 'printLine' function from the Print Module");
+    let style2 = document.createElement('style');
+    style2.textContent = claimPopup.popUpStyle;
+
+    const reactRoot = this.attachShadow({ mode: 'open' });
+    reactRoot.appendChild(mountPoint);
+    reactRoot.appendChild(style2);
+
+    const reactRoot2 = this.attachShadow({ mode: 'open' }).appendChild(s);
+
+    const jss = create({
+      ...jssPreset(),
+      insertionPoint: reactRoot,
+      insertionPoint: reactRoot2,
+    });
+
+    ReactDOM.render(
+      <StylesProvider jss={jss}>
+        <ContentScript />
+      </StylesProvider>,
+      mountPoint
+    );
+  }
+}
+
+const initWebComponent = function () {
+  customElements.define('react-extension-container', ReactExtensionContainer);
+
+  const app = document.createElement('react-extension-container');
+  app.id = 'react-extension-container';
+  document.documentElement.appendChild(app);
+};
+
+initWebComponent();
